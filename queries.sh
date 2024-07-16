4f1d41a5529d72afe564b48163a635091a3dba08
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 16:51:47 +0200

    feat: Added query to calculate the total number of goals scored by both teams combined

--- queries.sh
+++ queries.sh
@@ -7,3 +7,6 @@ PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c
 echo -e "\nTotal number of goals in all games from winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
 
+echo -e "\nTotal number of goals in all games from both teams combined:"
+echo "$($PSQL "SELECT SUM(winner_goals + opponent_goals) FROM games")"
+
