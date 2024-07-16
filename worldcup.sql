858146b9a88800926c7d922a09cfb392413c8b92
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 16:45:22 +0200

    feat: games table references fk

--- worldcup.sql
+++ worldcup.sql
@@ -279,7 +279,8 @@ ALTER TABLE ONLY public.games
 --
 -- Name: games games_winner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
-
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT games_winner_id_fkey FOREIGN KEY (winner_id) REFERENCES public.teams(team_id)
 
 --
 -- PostgreSQL database dump complete
