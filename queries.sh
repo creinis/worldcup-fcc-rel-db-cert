5f2d10b9c75768439e56a537d412b6db584088b8
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 16:50:54 +0200

    fest: db connection

---
+++ queries.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
