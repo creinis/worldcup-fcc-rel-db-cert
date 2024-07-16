497c900e9f7ec3754948a5f6ee448a3e2bc4011f
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 16:51:26 +0200

    feat: Added query to calculate the total number of goals scored by winning teams

--- queries.sh
+++ queries.sh
@@ -4,3 +4,6 @@ PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
+echo -e "\nTotal number of goals in all games from winning teams:"
+echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
+
