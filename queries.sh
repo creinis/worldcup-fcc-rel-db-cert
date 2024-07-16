c65df144f39f9e5f90d130d8d366a9140af4dce9
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 16:55:16 +0200

    feat: Added query to find the winner of the 2018 tournament

--- queries.sh
+++ queries.sh
@@ -25,3 +25,6 @@ echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
 echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
+echo -e "\nWinner of the 2018 tournament team name:"
+echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id = games.winner_id WHERE year=2018 AND round='Final'")"
+
