585987780671757bc74385f6655ca2dedd4e2863
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 16:34:16 +0200

    feat: create sequence for id in games table

--- worldcup.sql
+++ worldcup.sql
@@ -65,7 +65,15 @@ ALTER TABLE public.games OWNER TO freecodecamp;
 --
 -- Name: games_game_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
 --
+CREATE SEQUENCE public.games_game_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
+ALTER TABLE public.games_game_id_seq OWNER TO freecodecamp;
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
